c582cceea031d141de7941898975f914c9ac5b2e
Andrew Sklyarov <andrew.v.sklyarov@gmail.com>
2026-01-03 18:24:40 -0500

    First real commit with, you know, files

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,67 @@
+import { React } from 'react';
+import { useState } from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <div className='home-page'>
+            <div className='top-bar'>
+                <h1 className='title'>GoTutor.ai</h1>
+            </div>
+            <h1 className='catchphrase'>
+                Your private tutor, on demand.
+            </h1>
+            <button className='sign-in'>Create Account or Sign In</button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p>yo</p>
+        </div>
+    );
+}
+
+export default Home;
