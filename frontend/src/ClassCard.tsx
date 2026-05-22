c5e6e214030bdf1038597fd3e277a67f83389a7e
Andrew Sklyarov <andrew.v.sklyarov@gmail.com>
2026-05-22 15:37:23 -0400

    Add class card component

--- frontend/src/ClassCard.tsx
+++ frontend/src/ClassCard.tsx
@@ -1,5 +1,12 @@
-function ClassCard() {
-    
+import './ClassCard.css';
+
+function ClassCard({ classTitle, supervisorName }: { classTitle: string, supervisorName: string }) {
+    return(
+        <div className='class-card'>
+            <h2>{classTitle}</h2>
+            <h3>{supervisorName}</h3>
+        </div>
+    )
 };
 
 export default ClassCard;
--- frontend/src/UserDashboardSignedIn.tsx
+++ frontend/src/UserDashboardSignedIn.tsx
@@ -1,4 +1,5 @@
 import { type User } from 'firebase/auth';
+import ClassCard from './ClassCard';
 import './UserDashboard.css';
 
 function UserDashboardSignedIn({ user }: { user: User }) {
@@ -7,6 +8,9 @@ function UserDashboardSignedIn({ user }: { user: User }) {
         <div className='user-dashboard'>
             <h1>Welcome, {user.displayName}</h1>
             <h2>Your classes:</h2>
+            <div className='classes-display-container'>
+                
+            </div>
         </div>
     ); 
 };
