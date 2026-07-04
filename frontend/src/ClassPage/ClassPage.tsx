648f59d5a6379ddff74556d01b0eb7ba3c720784
Andrew Sklyarov <andrew.v.sklyarov@gmail.com>
2026-07-04 17:19:08 -0400

    Refactor ClassPage to have new user name logic

--- frontend/src/ClassPage/ClassPage.tsx
+++ frontend/src/ClassPage/ClassPage.tsx
@@ -8,15 +8,22 @@ function ClassPage() {
     const { classId } = useParams<{ classId: string }>();
     const userCtx = useContext(UserContext);
     const navigate = useNavigate();
+    const user = userCtx?.user;
+
+    if(!user) {
+        return null;
+    }
+
+    const name = user.user_metadata?.full_name || user.email || "User";
 
     // Update this to the actual class name retrieved from db
-    const name = "Class " + classId; 
+    const clsName = "Class " + classId; 
 
     return(
         <div>
             {userCtx?.user /*true*/ ? (
                 // Add your dashboard content here when user is authenticated
-                <ClassPageSignedIn cls={name} user={userCtx?.user.displayName} classId={classId ?? ''}/>
+                <ClassPageSignedIn cls={clsName} user={name} classId={classId ?? ''}/>
             ) : 
                 <SignedOut nav={navigate}/>
             }
